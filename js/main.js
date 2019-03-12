d523d9f436ec10236ec5a44d8d3e20ec2408bc54
Adam Fister <arfister@gmail.com>
2019-03-12 11:28:29 -0400

    implement navbar js functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-var person = {};
+$(window).on("scroll", function () {
+    var scrollPos = $(window).scrollTop();
+    $('.navbar-default').addClass('top-of-page');
+
+});
+
+$('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+$(document).ready(function(){
+$('.navbar-nav .nav-item').click(function(){
+$('.navbar-nav .nav-item').removeClass('active');
+$(this).addClass('active');
+})
+});
