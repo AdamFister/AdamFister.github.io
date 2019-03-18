99a07455dbea1b9245ebc114fae179f1b9719081
Adam Fister <arfister@gmail.com>
2019-03-18 16:40:24 -0400

    icon hover colors

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+Vue.component('date', {
+    computed: {
+        formatDate() {
+            var today = new Date();
+            var dd = today.getDate();
+            var yyyy = today.getFullYear();
+
+            var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+            return currDate = (day[today.getDay()] + ' ' + month[today.getMonth()] + ' ' + dd + ', ' + yyyy);
+        }
+    },
+    template: `<h1>{{ formatDate }}</h1>`
+})
+
+var app = new Vue({
+    el: '#app'
+})
+
 $(window).on("scroll", function () {
     var scrollPos = $(window).scrollTop();
     $('.navbar-default').addClass('top-of-page');
